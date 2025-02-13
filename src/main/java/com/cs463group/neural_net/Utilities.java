356f51114cbddc88ec58f59a91e346da2afd9381
gjin <gjinrexhaj1@gmail.com>
2025-02-12 20:39:30 -0500

    feat: add sigmoid derivative function, add mean square loss fucntion

--- src/main/java/com/cs463group/neural_net/Utilities.java
+++ src/main/java/com/cs463group/neural_net/Utilities.java
@@ -1,6 +1,8 @@
 package com.cs463group.neural_net;
 
 
+import java.util.List;
+
 /**
  *  Utilities.java
  *  Created on 2/11/2025
@@ -11,5 +13,21 @@ public class Utilities {
     // Sigmoid activation function
     public static double sigmoid(double input) {
         return 1 / (1 + Math.exp(-input));
+    }
+
+    // Derivative of sigmoid activation function
+    public static double sigmoidDeriv(double in){
+        double sigmoid = Utilities.sigmoid(in);
+        return sigmoid * (1 - in);
+    }
+
+    // Mean square loss function
+    public static Double meanSquareLoss(List<Double> correctAnswers, List<Double> predictedAnswers){
+        double sumSquare = 0;
+        for (int i = 0; i < correctAnswers.size(); i++){
+            double error = correctAnswers.get(i) - predictedAnswers.get(i);
+            sumSquare += (error * error);
+        }
+        return sumSquare / (correctAnswers.size());
     }
 }
