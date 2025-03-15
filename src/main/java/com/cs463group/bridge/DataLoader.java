e11e3664ea73eaea8c228d8f2a7d3d7b5f6cd32e
gjin <gjinrexhaj1@gmail.com>
2025-03-15 10:29:21 -0400

    feat(bridge): create dataLoader.java

--- src/main/java/com/cs463group/bridge/DataLoader.java
+++ src/main/java/com/cs463group/bridge/DataLoader.java
@@ -1,6 +1,6 @@
 package com.cs463group.bridge;
 
-// TODO: Implement a dataLoader in bridge that loads data in a parametrized fashion
+// TODO: rigorously test DataLoader
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,24 +13,33 @@ import java.util.stream.Collectors;
  *  DataLoader.java
  *  Created on 3/11/2025
  *  Defines an adaptive and flexible data loader, which reads and formats data from
- *  a csv or plaintext file such that the backend can interact with it.
+ *  a csv file such that the backend can interact with it.
  */
 
 public class DataLoader {
 
-    // temp main method to test functionality
+    // main method test fixture
     public static void main(String[] args) {
 
+        // Tweak parameters based on neural net attributes
+        int inputDimensionality = 4;
+        int numOfOutputNeurons = 2;
 
-        List<List<Double>> records = new ArrayList<>();
-        records = loadData("test-data/weight-height-gender/4-entries/Source-data.csv");
-        System.out.println("RECORD SIZE: " + records.size());
-        for(int i = 0; i < records.size(); i++){
-            System.out.println(records.get(i));
-        }
+        List<List<Double>> Records = new ArrayList<>();
+        //Records = loadData("test-data/weight-height-gender/4-entries/Source-data.csv");
+        Records = loadData("src/main/java/com/cs463group/bridge/DataLoaderTest.csv");
 
-        //dataInputs = seperateInputs(records);
-        //dataAnswers = seperateAnswers(records);
+        System.out.println();
+        System.out.println("INPUT DIMEN: " + inputDimensionality);
+        System.out.println("NUM OUTNEUR: " + numOfOutputNeurons);
+        System.out.println("RECORD SIZE: " + Records.size());
+
+        List<List<Double>> DataInputs = seperateInputs(Records, inputDimensionality);
+        List<List<Double>> DataAnswers = seperateAnswers(Records, numOfOutputNeurons);
+
+        System.out.println("Records:     " + Records);
+        System.out.println("dataInputs:  " + DataInputs);
+        System.out.println("dataAnswers: " + DataAnswers);
     }
 
     // static method to load values from file into 2d string list
@@ -57,38 +66,36 @@ public class DataLoader {
         return formattedData;
     }
 
-    //public static
+    public static List<List<Double>> seperateInputs(List<List<Double>> fileContents, int inputDimensionality) {
+        List<List<Double>> inputs = new ArrayList<>();
 
-    // draft: MAKE BELOW CODE ADAPTIVE - use num of input datapoint and num of output neurons as parameters for
-    // data size and handling
-     /*
-        Scanner scan;
-        File file = new File("test-data/weight-height-gender/Source-Data.txt");
-        try {
-            scan = new Scanner(file);
-            scan.useDelimiter("[,\\n]");
-            int i = 0;
-
-            while (scan.hasNext()) {
-                i++;
-                String token = scan.next();
-                if (i == 1) {
-                    // data add 1 and next
-                    String nextToken = scan.next();
-                    data.add(Arrays.asList(Double.parseDouble(token), Double.parseDouble(nextToken)));
-                    System.out.print(i + ": ");
-                    System.out.println(token + ", " + nextToken);
-                } else if (i == 2) {
-                    // do nothing
-                    System.out.print(i + ": ");
-                    answers.add(Double.parseDouble(token));
-                    System.out.println(token);
-                    i = 0;
+        for(int i = 0; i < fileContents.size(); i++) {
+            List<Double> row = new ArrayList<>();
+            for (int j = 0; j < fileContents.get(i).size(); j++) {
+                if (j < inputDimensionality) {
+                    row.add(fileContents.get(i).get(j));
+                } else if (j > inputDimensionality) {
+                    break;
+                } else {
+                    inputs.add(row);
                 }
             }
-
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
         }
-         */
+        return inputs;
+    }
+
+    public static List<List<Double>> seperateAnswers(List<List<Double>> fileContents, int numOfOutputNeurons) {
+        List<List<Double>> answers = new ArrayList<>();
+
+        for(int i = 0; i < fileContents.size(); i++) {
+            List<Double> row = new ArrayList<>();
+            for (int j = 0; j < fileContents.get(i).size(); j++) {
+                if (j > fileContents.get(i).size()-1 - numOfOutputNeurons) {
+                    row.add(fileContents.get(i).get(j));
+                }
+            }
+            answers.add(row);
+        }
+        return answers;
+    }
 }
